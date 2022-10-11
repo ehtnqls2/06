b670a72f39549d1d4f87ee5d09fd36470e8f172d
ehtnqls2 <113421745+ehtnqls2@users.noreply.github.com>
2022-10-11 17:36:49 +0900

    05

--- main.c
+++ main.c
@@ -2,18 +2,32 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
-void square(int a)
+int factorial(int x)
 {
-	a= a*a;
+	int res=1;
+	int i;
+	for(i=1;i<=x;i++)
+		res=res*i;
+	return res;	
+}
+int combination(int n, int r)
+{
+	int comb;
+	comb=factorial(n)/(factorial(n-r)*factorial(r));
+	return comb;
 }
 
+
+
 int main(int argc, char *argv[]) {
+	int a,n,r;
+	printf("n의 값을 입력하세요");
+	scanf("%i",&n);
+	printf("r의 값을 입력하세요");
+	scanf("%i",&r);
 	
-	int a=2;
-	square(a);
-	printf("a=%i\n",a);
+	a=combination(n,r);
+	printf("결과는 %i 입니다",a);
 	
 	return 0;
 }
