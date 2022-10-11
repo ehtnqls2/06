5b170c14665bb9276774c6979ec3ac2f158d2cfe
ehtnqls2 <113421745+ehtnqls2@users.noreply.github.com>
2022-10-11 17:01:21 +0900

    03

--- main.c
+++ main.c
@@ -4,18 +4,31 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-void star()
+int sumTwo(int a, int b)
 {
-	int i;
-	for(i=0;i<10;i++)
-		printf("*");
+	return (a+b);
+}
+int square(int n)
+{
+	return (n*n);
+}
+int get_max(int x, int y)
+{
+	if(x>y)
+		return x;
+	else
+		return y;
 	
 }
 
 int main(int argc, char *argv[]) {
-	star();
-	star();
-	star();
+	
+	int res_sum, res_square,res_get_max;
+	res_sum=sumTwo(2,3);
+	res_square=square(5);
+	res_get_max=get_max(10,-20);
+	
+	printf("%i %i %i \n", res_sum,res_square,res_get_max);
 	
 	return 0;
 }
